6f322f30635643e1f0bbf320f9feca0e5b9303e6
Maxim Shilo <maximshilo.dev@gmail.com>
2024-05-04 17:47:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -64,8 +64,7 @@ function App() {
                             let key = e.key
                             let value = e.target.value
                             let allowedKeys = '1 2 3 4 5 6 7 8 9'
-                            console.log(!allowedKeys.indexOf(key) === -1, 'allowedKyes')
-                            if (key != 'Backspace' && (value.length > 0 || allowedKeys.indexOf(key) === -1)) e.preventDefault()
+                            if (key !== 'Backspace' && (value.length > 0 || allowedKeys.indexOf(key) === -1)) e.preventDefault()
                             else return true
                           }}
                           onChange={(e) => {
